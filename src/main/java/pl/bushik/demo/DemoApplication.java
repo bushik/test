29fe42ddccff3b89a0757d5f647eb3838ce8122b
Karol Błażejowski <karol.blazejowski@eclipsegroup.co.uk>
2020-08-04 11:12:00 +0200

    add comment

--- src/main/java/pl/bushik/demo/DemoApplication.java
+++ src/main/java/pl/bushik/demo/DemoApplication.java
@@ -3,11 +3,17 @@ package pl.bushik.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//**
+* Demo application
+**/
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+
 	}
 
 }
